670ab909ccdce0c3635f6053178c9a62d032db94
Lily Chrisman <lilychrisman07@gmail.com>
2025-05-17 15:00:11 -0700

    half backed rewrite of structure

--- src/playset/playset.rs
+++ src/playset/playset.rs
@@ -5,6 +5,7 @@ use super::pset_format;
 
 #[derive(Hash, PartialEq, Eq, Clone)]
 pub struct Song {
+    pub name: String,
     pub genre: String,
     pub artist: String,
     pub album: Option<String>,
@@ -14,10 +15,11 @@ pub struct Song {
 }
 
 impl Song {
-    pub fn from_path<P: AsRef<Path>>(p: P) -> audiotags::Result<Self> {
+    pub fn from_path<P: AsRef<Path>>(p: P, name: String) -> audiotags::Result<Self> {
         let meta = Tag::new().read_from_path(&p)?;
         
         Ok(Self {
+            name,
             genre: meta.genre().unwrap_or("").to_owned(),
             artist: meta.artist().unwrap_or("").to_owned(),
             album: meta.album().map(|a| a.title.to_owned()),
@@ -27,23 +29,24 @@ impl Song {
     }
 }
 
-
-#[derive(Hash, PartialEq, Eq)]
-pub enum Transformation {
-    Union(String),
-    Intersection(String)
+pub struct TerminalSongset {
+    
 }
 
+pub struct NonTerminalSongset {
+    
+}
 
-pub enum Songset {
+pub enum SongSet {
     Terminal(HashSet<Song>),
-    NonTerminal((String, Vec<Transformation>))
+    NonTerminal(String)
 }
-impl Songset {
+
+impl SongSet {
     pub fn flatten(&self) -> HashSet<Song> {
         match self {
-            Songset::Terminal(set) => set.clone(),
-            Songset::NonTerminal((base, transforms)) => {
+            SongSet::Terminal(set) => set.clone(),
+            SongSet::NonTerminal(name) => {
                 todo!()
             },
         }
@@ -51,37 +54,64 @@ impl Songset {
     pub fn to_pset_string(&self) -> String {
         let mut out = String::new();
         match self {
-            Songset::Terminal(set) => {
-                out.push(pset_format::L_BRACE);
+            SongSet::Terminal(set) => {
+                out.push(pset_format::SET_START);
                 for song in set {
                     out.push_str(&song.path);
-                    out.push(pset_format::COMMA)
+                    out.push(pset_format::SEPERATOR)
                 }
                 out.pop();
-                out.push(pset_format::R_BRACE);
+                out.push(pset_format::SET_END);
             },
-            Songset::NonTerminal((base, transforms)) => {
-                
+            SongSet::NonTerminal(name) => {
+                out = name.clone();
+                out.push(pset_format::SEPERATOR);
             },
         }
         out
     }
+    pub fn from_pset_string(s: String) -> Self {
+        for c in s.chars() {
+            
+        }
+
+        todo!()
+    }
 }
 
+pub enum SongTree {
+    Operation(char, SongTreeNode),
+    Set(SongSet),
+}
+
+pub struct SongTreeNode {
+    lhs: Box<SongTree>,
+    rhs: Box<SongTree>,
+}
+
+impl SongTree {
+    pub fn to_pset_string(&self) -> String {
+        match self {
+            SongTree::Operation(op, song_tree_node) => {
+                todo!()
+            },
+            SongTree::Set(song_set) => {
+                song_set.to_pset_string()
+            },
+        }
+    }
+}
 
 pub struct Playset {
     pub name: String,
-    pub set: Songset
+    pub songs: SongTree,
 }
 impl Playset {
     pub fn write_to_file<P: AsRef<Path>>(&self, song_library: P) -> io::Result<()> {
         let mut output_path = song_library.as_ref().to_str().unwrap().to_owned();
         output_path.push_str(&self.name);
 
-        let mut out = String::new();
-        for song in self.set.flatten() {
-            
-        }
+        let mut out = self.songs.to_pset_string();
 
         fs::write(output_path, out)?;
 
@@ -91,7 +121,7 @@ impl Playset {
     pub fn empty_terminal(name: String) -> Self {
         Self {
             name,
-            set: Songset::Terminal(HashSet::new())
+            set: SongSet::Terminal(HashSet::new())
         }
     }
 }
@@ -107,11 +137,14 @@ impl Library {
 
         let universal_set = universal_dir
             .map(|f| f.unwrap().path())
-            .map(|f| Song::from_path(f).unwrap())
-            .collect::<HashSet<Song>>();
+            .map(|p| {
+                let p2 = p.clone();
+                let f_name = p.file_name().unwrap().to_str().unwrap();
+                Song::from_path(p2, f_name.to_owned()).unwrap()
+            }).collect::<HashSet<Song>>();
         let universal_set = Playset {
             name: "U".to_owned(),
-            set: Songset::Terminal(universal_set)
+            set: SongSet::Terminal(universal_set)
         };
 
         for file in subset_dir
--- src/playset/pset_format.rs
+++ src/playset/pset_format.rs
@@ -1,3 +1,7 @@
-pub const COMMA: char = 0x01 as char;
-pub const L_BRACE: char = 0x02 as char;
-pub const R_BRACE: char = 0x03 as char;
+pub const SEPERATOR: char = 0x01 as char;
+pub const SET_START: char = 0x02 as char;
+pub const SET_END: char = 0x03 as char;
+
+pub const UNION: char = 0x10 as char;
+pub const INTERSECTION: char = 0x11 as char;
+pub const DIFFERENCE: char = 0x12 as char;
